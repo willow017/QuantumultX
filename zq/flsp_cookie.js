808c8bbc8a7efe2f1a6b14bb1b9bc32d57c10f5d
willow017 <79699283+willow017@users.noreply.github.com>
2022-03-27 23:09:05 +0800

    Update flsp_cookie.js

--- zq/flsp_cookie.js
+++ zq/flsp_cookie.js
@@ -7,7 +7,7 @@ instruction: [mitm]
              hostname = kandian.wkandian.com
              [rewrite_local]
              ^https://kandian\.wkandian\.com/v5/article/complete\.json url script-request-body https://raw.githubusercontent.com/willow017/QuantumultX/main/zq/zqyd_cookie.js
-             ^https://kandian\.wkandian\.com/V17/NewTaskIos/recordNum\.json url script-request-body https://raw.githubusercontent.com/willow017/QuantumultX/main/zq/flsp_cookie.js
+             ^https://kandian.wkandian.com/V17/NewTaskIos/recordNum.json url script-request-body https://raw.githubusercontent.com/willow017/QuantumultX/main/zq/flsp_cookie.js
 
 */
 let flspBody = $prefs.valueForKey('flsp_auto');
@@ -18,32 +18,32 @@ const flsp = '福利视频';
 
 //获取阅读body
 if (isGetbody = typeof $request !==`undefined`) {
-  flspbody();
+  Getbody();
   $done({})
 } 
 
 
-function flspbody() {
-  if ($request && $request.url,match(/\/NewTaskIos\/recordNum/)){
-    if ($request.url.match(/recordNum/)){
-      bodyVal = $request.body;
-    } else {
-      bodyVal = $request.url.split("?")[1];
-    };
-    if (flspBody) {
-      if (flspBody.indexOf(bodyVal)>-1){
-        console.log("此阅读请求已存在，本次跳过");
-      } else if (flspBody.indexOf(bodyVal)==-1) {
-        flspBodys = flspBody + "&" + bodyVal;
-        $prefs.setValueForKey(flspBodys, 'flsp_auto');
-        console.log(`中青看点获取福利视频: 成功, flspBodys: ${bodyVal}`);
-        bodys = flspbodys.split("&");
-        $notify(title, "获取第" + bodys.length + "个福利视频请求: 成功🎉", ``);
-      }
-    } else {
-      $prefs.setValueForKey(bodyVal, 'flsp_auto');
-      console.log(`中青看点获取阅读: 成功, flspBodys: ${bodyVal}`);
-      $notify(title, `获取第一个福利视频请求: 成功🎉`, ``);
+function Getbody() {
+    if ($request && $request.url.match(/\/NewTaskIos\/recordNum/)) {
+       if($request.url.match(/recordNum/)){
+          bodyVal = $request.body
+       } else {
+        bodyVal = $request.url.split("?")[1]
+        };
+        if (flspBody) {
+            if (flspBody.indexOf(bodyVal) > -1) {
+                console.log("此阅读请求已存在，本次跳过")
+            } else if (flspBody.indexOf(bodyVal) == -1) {
+                flspBodys = flspBody + "&" + bodyVal;
+                $prefs.setValueForKey(flspBodys, 'flsp_auto');
+                console.log(`中青看点获取阅读: 成功, flspBodys: ${bodyVal}`);
+                bodys = flspBodys.split("&")
+                $notify(title, "获取第" + bodys.length + "个阅读请求: 成功🎉", ``)
+            }
+        } else {
+            $prefs.setValueForKey(bodyVal, 'flsp_auto');
+            console.log(`中青看点获取阅读: 成功, flspBodys: ${bodyVal}`);
+            $notify(title, `获取第一个阅读请求: 成功🎉`, ``)
+        }
     }
-  }
 }
